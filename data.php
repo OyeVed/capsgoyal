e2966b386f1ed58cacf4589a26ca46d82ce11a0f
unknown <ayush02av@gmail.com>
2022-08-19 18:51:13 +0530

    sorting

--- data.php
+++ data.php
@@ -1,4 +1,105 @@
 <?php
+// A PHP program to sort an array according
+// to the order defined by another array
+ 
+/* A Binary Search based function to find index
+of FIRST occurrence of x in arr[]. If x is not
+present, then it returns -1 */
+function first(&$arr, $low, $high, $x, $n)
+{
+    if ($high >= $low)
+    {
+        $mid = intval($low + ($high - $low) / 2);
+        if (($mid == 0 || $x > $arr[$mid - 1]) &&
+                               $arr[$mid] == $x)
+            return $mid;
+        if ($x > $arr[$mid])
+            return first($arr, ($mid + 1), $high, $x, $n);
+        return first($arr, $low, ($mid - 1), $x, $n);
+    }
+    return -1;
+}
+ 
+// Sort A1[0..m-1] according to the order
+// defined by A2[0..n-1].
+function sortAccording(&$A1, &$A2, $m, $n)
+{
+    // The temp array is used to store a copy
+    // of A1[] and visited[] is used mark the
+    // visited elements in temp[].
+    $temp = array_fill(0, $m, NULL);
+    $visited = array_fill(0, $m, NULL);
+    for ($i = 0; $i < $m; $i++)
+    {
+        $temp[$i] = $A1[$i];
+        $visited[$i] = 0;
+    }
+ 
+    // Sort elements in temp
+    sort($temp);
+ 
+    $ind = 0; // for index of output which is sorted A1[]
+ 
+    // Consider all elements of A2[], find
+    // them in temp[] and copy to A1[] in order.
+    for ($i = 0; $i < $n; $i++)
+    {
+        // Find index of the first occurrence
+        // of A2[i] in temp
+        $f = first($temp, 0, $m - 1, $A2[$i], $m);
+ 
+        // If not present, no need to proceed
+        if ($f == -1) continue;
+ 
+        // Copy all occurrences of A2[i] to A1[]
+        for ($j = $f; ($j < $m &&
+             $temp[$j] == $A2[$i]); $j++)
+        {
+            $A1[$ind++] = $temp[$j];
+            $visited[$j] = 1;
+        }
+    }
+ 
+    // Now copy all items of temp[] which
+    // are not present in A2[]
+    for ($i = 0; $i < $m; $i++)
+        if ($visited[$i] == 0)
+            $A1[$ind++] = $temp[$i];
+}
+ 
+// Utility function to print an array
+function printArray(&$arr, $n)
+{
+    for ($i = 0; $i < $n; $i++)
+        echo $arr[$i] . " ";
+    echo "\n";
+}
+
+$order = [
+51,
+207,
+208,
+4,
+209,
+211,
+210,
+2,
+3,
+5
+];
+
+$names = [
+    'Not Started',
+    'Backup NR',
+    'Backup Recd',
+    'In Progress',
+    'Submitted',
+    'Challan Pending',
+    'Uploaded In Sw',
+    'Feedback',
+    'Testing',
+    'Complete'
+];
 
 $DB_HOSTNAME = 'capsgoyal.com';
 $DB_USERNAME = 'capsgr9s_excel';
@@ -97,6 +198,13 @@ while($row = $result->fetch_assoc()){
     }
     
     array_push($data, $data_element);
+}
+
+// Driver Code
+sortAccording($status_headings, $order, sizeof($status_headings), sizeof($order));
+
+foreach ($status_headings as $key => $value) {
+    $status_heading_names[$key] = $names[array_search($value, $order)];
 }
 
 header('Content-Type: application/json');
